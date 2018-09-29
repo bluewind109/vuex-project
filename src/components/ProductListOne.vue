5f89c20c78f4fe5f8a06093c3ebabfe5d3d0120c
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2018-09-29 19:24:14 +0700

    Using computed for Vuex

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -4,7 +4,7 @@
     <ul>
       <li v-for="product in products">
         <span class="name">{{product.name}}</span>
-        <span class="price">{{product.price}}</span>
+        <span class="price">${{product.price}}</span>
       </li>
     </ul>
   </div>
@@ -12,34 +12,33 @@
 
 <script>
 export default {
-  props: ['products'],
-  data() {
-    return {
-
-    };
+  computed: {
+    products() {
+      return this.$store.state.products;
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
-#product-list-one{
-    background: #FFF8B1;
-    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
-    margin-bottom: 30px;
-    padding: 10px 20px;
+#product-list-one {
+  background: #fff8b1;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
 }
-#product-list-one ul{
-    padding: 0;
+#product-list-one ul {
+  padding: 0;
 }
-#product-list-one li{
-    display: inline-block;
-    margin-right: 10px;
-    margin-top: 10px;
-    padding: 20px;
-    background: rgba(255,255,255,0.7);
+#product-list-one li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
 }
-.price{
-    font-weight: bold;
-    color: #E8800C;
+.price {
+  font-weight: bold;
+  color: #e8800c;
 }
 </style>
